899b62634a593b6512188c752c480a7ce1f14fd9
Sanusi Hassan <sanusihassan122@gmail.com>
2025-04-17 09:25:07 +0100

    integrating pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import pwa from '@vite-pwa/astro';
 
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(),
+  pwa({
+    strategies: 'generateSW',
+    registerType: 'autoUpdate',
+    manifestFilePath: 'public/manifest.json',
+    includeManifestIcons: false,
+    manifest: false,
+  })
+  ],
   site: "https://www.pdfequips.com",
-  // 
   vite: {
     ssr: {
       noExternal: ['react-icons']
